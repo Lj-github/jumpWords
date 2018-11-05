fdd63c10620959aee869cf473c7313e9b9d60b86
刘江 <1433727556@qq.com>
2018-11-05 15:03:13 +0800

    截图base64

--- client2D/jumpCocos/src/App.js
+++ client2D/jumpCocos/src/App.js
@@ -17,9 +17,15 @@ var GameLayer = cc.Layer.extend({
         this.helloLabel2.setColor(cc.color(0, 178, 111))
         this.helloLabel.setColor(cc.color(0, 233, 12))
         this.time = 0
-        this.schedule(this.run, 1/5)
+        this.schedule(this.run, 1 / 5)
 
+        this.sprite = new cc.Sprite(res.HelloWorld_png)
+        this.sprite.setAnchorPoint(0.5, 0.5)
+        this.sprite.setPosition(size.width / 2, size.height / 2)
+        this.sprite.setScale(size.height / this.sprite.getContentSize().height)
+        this.addChild(this.sprite, 0)
 
+        this.getScreenShotInCanvasModele()
         return true;
     },
     run: function (time) {
@@ -28,15 +34,37 @@ var GameLayer = cc.Layer.extend({
         this.helloLabel.y = Math.abs(posY * .6)
 
 
-        //获取 canvas base64 data
-        var canvas =  document.getElementById("gameCanvas");
-        if(canvas){
-            console.log( canvas.toDataURL())
-        }
+    },
 
+    //在project json  "renderMode":1, canvas  0 webgl
+    //获取 canvas base64 data   //canvas 使用 toDataURL 可以完美截图
+    getScreenShotInCanvasModele: function () {
+        return document.getElementById("gameCanvas").toDataURL()
+    },
+    getScreenShotInCanvasModele: function () {
+        var listener = cc.eventManager.addListener({
+            event: cc.EventListener.CUSTOM,
+            eventName: cc.Director.EVENT_AFTER_DRAW,
+            callback: function (event) {
+                function grab(x, y, width, height) {
+                    var arrayBuffer = new Uint8Array(width * height * 4);
+                    cc.game._renderContext.readPixels(x, y, width, height, cc.game._renderContext.RGBA, cc.game._renderContext.UNSIGNED_BYTE, arrayBuffer);
+                    var clampArray = new Uint8ClampedArray(arrayBuffer, 0, arrayBuffer.length);
+                    var imageData = new ImageData(clampArray, width, height);
+                    var tempCanvas = document.createElement('canvas');
+                    document.appendChild(tempCanvas)
+                    tempCanvas.getContext('2d').putImageData(imageData, 0, 0, 0, 0, cc.game._renderContext.drawingBufferWidth, cc.game._renderContext.drawingBufferHeight);
+                    console.log(tempCanvas.toDataURL());
+                }
 
+                grab(0, 0, cc.game._renderContext.drawingBufferWidth, cc.game._renderContext.drawingBufferHeight);
+                //grab(0, 0, cc.winSize.width, cc.winSize.height);
+
+                cc.eventManager.removeListener(listener);
+                listener = undefined;
+            }
+        }, cc.Director._getInstance().getRunningScene());// 这是 webgl 渲染下 的可以 捕捉 内容
     }
-
 
 });
 
