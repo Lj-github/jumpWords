770ad96f3321d5a681e9ac97bec77bb766263105
liujiang <1433727556@qq.com>
2019-01-05 21:33:15 +0800

    tools update

---
+++ pySocket/http/view/postview.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/24 上午11:06
+from flask import render_template, request, make_response
+from pySocket.http import app
+from flask import jsonify
+import json
+from pySocket.http.lib import lzstring
+x = lzstring.LZString()
+
+@app.route('/post/', methods=['POST'])
+def postTest():
+    username = request.values['username']  # json['username']
+    content = request.values['content']  # .json['']
+    print("username " + username)
+    print("content " + content)
+    loader = {}
+    loader['dd'] = 5
+    return json.dumps(loader), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+@app.route('/mydict', methods=['GET', 'POST'])
+def mydict():
+    d = {'name': 'xmr', 'age': 18}
+    return jsonify(d)
+
+
+@app.route('/postSaveTxt/', methods=['POST'])
+def postSaveTxt():
+    username = request.values['username']
+    #content = x.decompress(request.values['content'])
+    content =request.values['content']
+    print('username  ' + username)
+    with open("static/txt/" + username, 'w') as f:
+        f.write(content)
+    loader = {}
+    loader['dd'] = 'success'
+    return json.dumps(loader), 200, {'Content-Type': 'application/json; charset=utf-8'}
