5f5702d4ce0f565b24e0932df2ab3b0a40b6fb58
liujiang <1433727556@qq.com>
2019-01-08 10:10:06 +0800

    skew bug

--- pySocket/http/static/jumpCocos/src/pannel/ImgRander.js
+++ pySocket/http/static/jumpCocos/src/pannel/ImgRander.js
@@ -41,20 +41,23 @@ var ImgRander = cc.Layer.extend({
             let drawNode = new cc.DrawNode();
             drawNode.clear();//清除节点缓存drawNode.ctor();//构造函数
             drawNode.drawRect(cc.p(i * 20, 0),
-                cc.p(i * 20 + 18, 100),
-                Union.getRandomColor());
+                cc.p(i * 20 + 18, 1000), // 设置的 大一些 不知道为什么 可能是bug  在  scale 设置 超过1 时候 同时 修改 scale  和 skew  会出现 偏移
+                Union.getRandomColor(), 0);
             drawNode.setAnchorPoint(0, 0)
             this['img' + i] = drawNode
             this.addChild(drawNode)
+            drawNode.setScaleY(.1)
             if (i === 0) {
                 window["drawNode"] = drawNode
+                drawNode.setSkewX(drawNode.getScaleY() * gt.rectSkewX)
+
+
             } else {
                 if (i !== 1) {
                     drawNode.setVisible(false)
                 }
             }
-            //设置 倾斜度  设置 k 值  还是 无法实现  不准确  y = kx + b //难道需要 b？
-            drawNode.setSkewX(gt.rectSkewX)
+            //TODO cocos skew  也是 角度 但是 可能有bug  还 得要 调试
         }
     },
 
