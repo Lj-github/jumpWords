d394500c0f2400dbe69c5995c524cfd96ade17f3
刘江 <1433727556@qq.com>
2019-02-21 16:16:17 +0800

    egret 粒子

--- client2D/jumpWords/src/layer/music/LayerReadMusci.ts
+++ client2D/jumpWords/src/layer/music/LayerReadMusci.ts
@@ -1,6 +1,6 @@
 /**
-     created by liujiang
-     time 2019/2/11 11:07
+ created by liujiang
+ time 2019/2/11 11:07
  */
 module game {
     export class LayerReadMusci extends eui.Component {
@@ -9,22 +9,31 @@ module game {
         musicReader: ReadMusic
         private nodeline: NodeLine
         private nodeCircleLine: NodeCircleLine
+        system:particle.ParticleSystem
         constructor() {
             super()
-            this.musicReader = new ReadMusic(App.mp3file)
+            //this.musicReader = new ReadMusic(App.mp3file)
             let nodeline = new NodeLine()
             nodeline.rotation = 180
             nodeline.x = 1120
             nodeline.y = 640
             this.nodeline = nodeline
             this.addChild(nodeline)
-            this.addEventListener(egret.Event.ENTER_FRAME, this.getBuf, this);
+            //this.addEventListener(egret.Event.ENTER_FRAME, this.getBuf, this);
             let nodeline1 = new NodeCircleLine()
             this.addChild(nodeline1)
             nodeline1.x = 300
             nodeline1.y = 400
             this.nodeCircleLine = nodeline1
+
+            var texture = RES.getRes("snow_png");
+            var config = RES.getRes("snow_json");
+            this.system = new particle.GravityParticleSystem(texture, config);
+            this.addChild(this.system)
+            this.system.start();
+
         }
+
         getBuf() {
             this.buff = this.musicReader.music.getBuff()
             if (this.buff) {
@@ -41,8 +50,8 @@ module game {
                     if (this.nodeline.lintArr[i]) {
                         this.nodeline.lintArr[i].scaleY = scaley * 5
                     }
-                    if (i == 20 ){
-                        this.scaleX = 1 + (scaley/50)
+                    if (i == 20) {
+                        this.scaleX = 1 + (scaley / 50)
                     }
 
                 }
