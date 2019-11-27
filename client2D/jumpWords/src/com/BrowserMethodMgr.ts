d8dea200b80b01471f0c25d357b04fc5172e6b86
liujiang <1433727556@qq.com>
2019-11-27 15:57:17 +0800

    品质

--- client2D/jumpWords/src/com/BrowserMethodMgr.ts
+++ client2D/jumpWords/src/com/BrowserMethodMgr.ts
@@ -9,9 +9,43 @@ module BrowserMethodMgr {
     export function saveBase64DataToBrowser(bast64Str: string) {
         if (window.J_Base64ToImageUtil) {
             fileId++
-            window.J_Base64ToImageUtil.base64ChangeImage(bast64Str, fileId + ".jpg")
+            window.J_Base64ToImageUtil.base64ChangeImage(bast64Str, this.compareNumStr(fileId, 6) + ".png")
         } else {
             console.log(bast64Str)
         }
+    }
+
+    export function compareNumStr(num: number, len: number) {
+        let numStr = String(num)
+        return leftPad(numStr, len)
+    }
+
+
+    /**
+     * 
+     * @param str 补0操作
+     * @param len 
+     */
+    export function leftPad(str, len) {
+        str = str + '' || '';
+        len = len || 0;
+        while (str.length < len) {
+            str = "0" + str;
+        }
+        return str;
+    }
+
+    /**
+     * 
+     * @param str 补0操作
+     * @param len 
+     */
+    export function rightPad(str, len) {
+        str = str + '' || '';
+        len = len || 0;
+        while (str.length < len) {
+            str += "0";
+        }
+        return str;
     }
 }
