a330d15a24dc5d6f04edfe4a163479028b75b3a3
liujiang <1433727556@qq.com>
2019-12-11 21:45:18 +0800

    shape

--- client2D/jumpWords/src/com/Gt.ts
+++ client2D/jumpWords/src/com/Gt.ts
@@ -205,15 +205,15 @@ module gt {
     }
 
     export function getLineFilter() {
-        var color: number = 0x33CCFF;        /// 光晕的颜色，十六进制，不包含透明度
-        var alpha: number = 0.8;             /// 光晕的颜色透明度，是对 color 参数的透明度设定。有效值为 0.0 到 1.0。例如，0.8 设置透明度值为 80%。
-        var blurX: number = 35;              /// 水平模糊量。有效值为 0 到 255.0（浮点）
-        var blurY: number = 35;              /// 垂直模糊量。有效值为 0 到 255.0（浮点）
-        var strength: number = 2;            /// 压印的强度，值越大，压印的颜色越深，而且发光与背景之间的对比度也越强。有效值为 0 到 255。暂未实现
-        var quality: number = egret.BitmapFilterQuality.HIGH;        /// 应用滤镜的次数，建议用 BitmapFilterQuality 类的常量来体现
-        var inner: boolean = false;            /// 指定发光是否为内侧发光，暂未实现
-        var knockout: boolean = false;            /// 指定对象是否具有挖空效果，暂未实现
-        var glowFilter: egret.GlowFilter = new egret.GlowFilter(color, alpha, blurX, blurY,
+        let color: number = 0x33CCFF;        /// 光晕的颜色，十六进制，不包含透明度
+        let alpha: number = 0.8;             /// 光晕的颜色透明度，是对 color 参数的透明度设定。有效值为 0.0 到 1.0。例如，0.8 设置透明度值为 80%。
+        let blurX: number = 150;              /// 水平模糊量。有效值为 0 到 255.0（浮点）
+        let blurY: number = 150;              /// 垂直模糊量。有效值为 0 到 255.0（浮点）
+        let strength: number = 10;            /// 压印的强度，值越大，压印的颜色越深，而且发光与背景之间的对比度也越强。有效值为 0 到 255
+        let quality: number = egret.BitmapFilterQuality.HIGH;        /// 应用滤镜的次数，建议用 BitmapFilterQuality 类的常量来体现
+        let inner: boolean = false;            /// 指定发光是否为内侧发光，暂未实现
+        let knockout: boolean = false;            /// 指定对象是否具有挖空效果，暂未实现
+        let glowFilter: egret.GlowFilter = new egret.GlowFilter(color, alpha, blurX, blurY,
             strength, quality, inner, knockout);
         return glowFilter
     }
@@ -255,7 +255,13 @@ module gt {
 
         return (temp5 + temp6) / (8 * Math.pow(A, 1.5));
     }
-
+    export function getRandomColor() {
+        return Number('0x' +
+            (function (color) {
+                return (color += '0123456789abcdef'[Math.floor(Math.random() * 16)])
+                    && (color.length == 6) ? color : arguments.callee(color);
+            })(''))
+    }
 
 
 }
--- client2D/jumpWords/src/layer/music/oneline/LayerMoreLine.ts
+++ client2D/jumpWords/src/layer/music/oneline/LayerMoreLine.ts
@@ -14,28 +14,42 @@ module game {
         _cernter: egret.Point
         buff: Music.musicbuffObj
         _count = 0
+        degre = 0
+        _ractList = []
+        _shapList = []
+        shapFocus: eui.Group
+        lineFocus: eui.Group
+        _stmp = 0
         initView() {
+            this.removeChildren()
+            window["dddd"] = this
+            this.shapFocus = new eui.Group()
+            this.lineFocus = new eui.Group()    
+            this.addChild(this.lineFocus)
+            this.addChild(this.shapFocus)
+            this.degre = MusicFactory.getMusicValueDegre() * 2
             this._count = MusicFactory.getVoicehighCount()
             this._cernter = new egret.Point(gt.size.width / 2, gt.size.height / 2)
-            this.removeChildren()
             let width = Math.floor(gt.size.width / this._count)
             let matrix = new egret.Matrix()
             matrix.createGradientBox(width, gt.size.height, Math.PI / 2);
-            let colors = [0x000000, gt.getHexColor(1, 32, 97), 0xff0000]
+            let colors = [0x00688B, 0x008B8B, 0x33CCFF]// [0x000000, gt.getHexColor(1, 32, 97), 0xff0000]
             let alphas = [1, 0.9, 0.8]
-            let ratios = [255 / 3, 255 / 3 * 2, 255]
+            let ratios = [255 / 8, 255 / 8 * 4, 255]
             for (let i = 0; i < this._count; i++) {
                 let ract = new egret.Shape()
                 ract.graphics.beginGradientFill(egret.GradientType.LINEAR, colors, alphas, ratios, matrix);
                 ract.graphics.drawRect(i * width, 0, width, gt.size.height);
                 ract.graphics.endFill();
-                this.addChild(ract);
+                this.lineFocus.addChild(ract);
+                this._ractList.push(ract)
             }
             this.buff = <Music.musicbuffObj>{}
             this.buff.step = 0
             this.buff.voicehigh = []
             this.scaleY = -1
             this.y = gt.size.height
+            this.lineFocus.filters = [gt.getLineFilter()]
         }
         getBuff() {
             this.buff = MusicFactory.getMusicBuff()
@@ -46,18 +60,45 @@ module game {
             this.sendBase64()
         }
         setLine() {
+            this._stmp++
             if (this.buff.voicehigh) {
-                let degre = MusicFactory.getMusicValueDegre()
-                for (let i = 0; i < this.$children.length; i++) {
-                    let rack = this.$children[i]
-                    let sy = Number(this.buff.voicehigh[i * this.buff.step]) / degre
+                for (let i = 0; i < this._ractList.length; i++) {
+                    let rack = this._ractList[i]
+                    let sy = Number(this.buff.voicehigh[i * this.buff.step]) / this.degre
                     sy = Math.abs(sy)
                     rack.scaleY = sy
                 }
+                if (this._stmp % 60 == 0) {
+                    this.randomShape()
+                }
             }
+            this._shapList.forEach((shap: egret.Shape) => {
+                shap.x++
+                shap.y++
+                if (shap.y > 2000) {
+
+                }
+            })
+
+
         }
         sendBase64() {
             BrowserMethodMgr.sendBase64ToJxBrowser()
+        }
+
+
+        randomShape() {
+            let Shape = this.createShape()
+            this._shapList.push(Shape)
+        }
+        createShape() {
+            let ract = new egret.Shape()
+            ract.graphics.beginFill(gt.getRandomColor());
+            ract.graphics.drawRect(300, 300, 300, 300);
+            ract.graphics.endFill();
+            this.shapFocus.addChild(ract);
+            return ract
+
         }
     }
 }
